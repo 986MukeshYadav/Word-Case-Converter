c5f82488f46e5315b6060212f79b23125f3a8833
Mukesh <myadav986@rku.ac.in>
2024-02-09 11:27:11 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,22 +1,82 @@
+import { useState } from 'react';
 import './App.css';
+import TextForm from './component/TextForm';
+import Alert from './component/Alert';
+import About from './component/About';
+import Navbar from './component/Navbar';
+// import TextForm from './component/TextForm';
+import {
+  BrowserRouter as Router,
+ Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
+  const [mode, setMode]=useState('light');
+  const [alert, setAlert]=useState('null');
+
+  const showAlert=(message,type)=>{
+      setAlert({
+        message:message,
+        type:type
+      })
+      setTimeout(()=>{
+        setAlert(null);
+        },1500);
+      
+  }
+  const removeBodyClasses=()=>{
+   
+    document.body.classList.remove('bg-light');
+    document.body.classList.remove('bg-dark');
+    document.body.classList.remove('bg-warning');
+    document.body.classList.remove('bg-danger');
+    document.body.classList.remove('bg-success');
+  }
+  
+
+  const toggleMode=(cls)=>{
+    removeBodyClasses();
+    document.body.classList.add('bg-'+cls)
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.background='#042743';
+      showAlert("Dark mode has been enabled","success");
+      document.title='TextUtils-Dark Mode';
+      // setInterval(()=>{
+      //   document.title = 'TextUtils is Amazing Mode';
+      // },2000);
+      // setInterval(()=>{
+      //   document.title = 'Install TextUtils Now';
+      // },1500);
+    }
+    else{
+      setMode('light');
+      document.body.style.background='white';
+      showAlert("Light mode has been enabled","success");
+      document.title='TextUtils-Light Mode';
+    }
+  }
   return (
    <>
-   <div className="blank">
-   <nav>
-    <li>Home</li>
-    <li>About</li>
-    <li>Contact</li>
-    <p>This is first project </p>
-   </nav>
+{/* <Navbar title="Mukesh" about="About us"/> */}
+<Router>
+<Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+<Alert alert={alert}/>
+<div className='container my-3'>
+<Routes>
+          <Route path="/about" 
+           element= {<About />}/>
+         
+          <Route path="/"
+         element={ <TextForm showAlert={showAlert} heading="Enter the text below" mode={mode}/>}/>
 
-      
-    
-
-   </div>
-   </>
+        </Routes>
+</div>
+</Router>
+  </>
   );
 }
 
 export default App;
+
---
+++ src/component/TextForm.js
@@ -0,0 +1,62 @@
+import React , {useState} from 'react'
+
+export default function TextForm(props) {
+    const handleUpClick=()=>{
+        //console.log("UpperCase was clicked" + text)
+        let newtext = text.toUpperCase();
+        setText(newtext);
+        props.showAlert("converted to uppercase","success")
+    }
+    const handleloClick=()=>{
+      //console.log("UpperCase was clicked" + text)
+      let newtext = text.toLowerCase();
+      setText(newtext);
+      props.showAlert("converted to lowercase","success")
+  }
+  const handleclearClick=()=>{
+    //console.log("UpperCase was clicked" + text)
+    let newtext = " ";
+    setText(newtext);
+    props.showAlert("cleared !","success")
+}
+const handlesentenceClick=()=>{
+  //console.log("UpperCase was clicked" + text)
+  let newtext = text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+  setText(newtext);
+  props.showAlert("converted to sentencecase","success")
+}
+
+
+    const handleOnChange = (event)=>{
+        //console.log("On changed")
+        setText(event.target.value)
+    }
+
+    const [text,setText]=useState('');
+    //text="new text"; //wrong way to change the state
+    // setText("new text");//correct way to change the state
+
+  return (
+    <>
+    <div className='container' style={{color:props.mode==='dark'?'white':'#042743'}}>
+        <h1>{props.heading}</h1>
+  <div className="form-group">
+    <textarea  className="form-control" id="myBox" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'#13466e':'white',color:props.mode==='dark'?'white':'#042743'}}/>
+  </div>
+  <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
+  <button className="btn btn-primary mx-1" onClick={handleloClick}>Convert to Lowercase</button>
+  <button className="btn btn-primary mx-1" onClick={handlesentenceClick}>Sentecnce Case</button>
+  <button className="btn btn-primary mx-1" onClick={handleclearClick}>Clear</button>
+ 
+    </div>
+   <div className="container my-2" style={{color:props.mode==='dark'?'white':'#042743'}}>
+    <h2>Text summary</h2>
+    <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length}words and {text.length}character</p>
+    <p>{0.008*text.split(" ").filter((element)=>{return element.length!==0}).length}minutes to read</p>
+    <h2>Preview</h2>
+    <p>{text.length>0?text:"Enter somethings in the textbox to preview"}</p>
+   
+   </div>
+    </>
+  )
+}
